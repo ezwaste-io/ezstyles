18f4cb4768daacd93e0a29c7eb0ddb48123a354d
David Paul Graham <dpgraham4401@gmail.com>
2025-11-13 08:34:41 -0500

    remove vite and typescript

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ]
-});
